d3c745764cce9a460c1a5e4a81c27938daf09b67
Seth Williamson <sethjwilliamson@gmail.com>
2022-10-13 18:32:54 -0600

    Update CardItem3d

--- src/lib-components/CardItem3d/src/CardItem3d.vue
+++ src/lib-components/CardItem3d/src/CardItem3d.vue
@@ -30,32 +30,20 @@ const backStyle = {
 * {
   box-sizing: border-box;
 }
-.container {
-  width: 1000px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-}
-button:focus {
-  outline: none;
-}
 .deck {
-  margin: 15px;
-  width: 270px;
-  height: 380px;
+  aspect-ratio: 643 / 969;
+  width: 300px;
   position: relative;
-  -webkit-perspective: 1000px;
-  perspective: 1000px;
-  font-family: verdana;
-  border-radius: 10px;
+  -webkit-perspective: 400px;
+  perspective: 400px;
 }
 .card {
   width: 100%;
   height: 100%;
   -webkit-transform-style: preserve-3d;
   transform-style: preserve-3d;
-  -webkit-transition: all 0.5s linear;
-  transition: all 0.5s linear;
+  -webkit-transition: all 1s var(--transition-flip);
+  transition: all 1s var(--transition-flip);
   border-radius: 10px;
 }
 .face {
@@ -64,8 +52,6 @@ button:focus {
   height: 100%;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
-  background-color: whitesmoke;
-  border-radius: 10px;
 }
 .back {
   overflow: hidden;
@@ -82,7 +68,6 @@ button:focus {
   height: 100%;
   z-index: -1;
   background-size: 100% 100%;
-  background: url('https://i.imgur.com/W3tra4F.gif');
   -webkit-transform: rotateY(180deg);
   -moz-transform: rotateY(180deg);
   -ms-transform: rotateY(180deg);
@@ -91,113 +76,15 @@ button:focus {
 }
 .front,
 .back {
-  transition: all 0.5s linear;
+  -webkit-transition: all 1s var(--transition-flip);
+  transition: all 1s var(--transition-flip);
 }
 .front {
-  background-image: url('https://i.imgur.com/W3tra4F.gif');
   z-index: 1;
-  box-shadow: 5px 5px 5px #aaa;
-}
-.back {
-  box-shadow: 0 0 0 #aaa;
 }
 .flipped,
 .hovercard:hover {
   -webkit-transform: rotateY(180deg);
   transform: rotateY(180deg);
-}
-.flipped .front,
-.hovercard:hover .front {
-  box-shadow: 0 0 0 #aaa;
-}
-.flipped .back,
-.hovercard:hover .back {
-  box-shadow: 5px 5px 5px #aaa;
-}
-.bottext {
-  position: absolute;
-  color: whitesmoke;
-  width: 100%;
-  height: 100px;
-  bottom: 0;
-  border-radius: 0 0 10px 10px;
-  text-align: center;
-  padding: 10px;
-}
-.bottext h2 {
-  margin: 0;
-}
-.bottext h3 {
-  margin: 10px 0;
-}
-.rad-button {
-  border: 2px solid gray;
-  color: gray;
-  width: 90px;
-  background-color: rgba(255, 255, 255, 0.3);
-  padding: 10px;
-  border-radius: 4px;
-  cursor: pointer;
-  position: absolute;
-  margin: 0 auto;
-  left: 0;
-  right: 0;
-  bottom: 110px;
-  -webkit-transition: all 0.5s linear;
-  transition: all 0.5s linear;
-}
-.rad-button:hover {
-  color: gray;
-  background-color: rgba(255, 255, 255, 1);
-}
-.front h2,
-.front h3 {
-  text-align: center;
-  color: whitesmoke;
-}
-.back h2 {
-  margin: 0;
-  border-radius: 10px 10px 0 0;
-  padding: 10px;
-  text-align: center;
-  color: whitesmoke;
-}
-.back ul {
-  text-align: center;
-  list-style: none;
-  padding: 10px;
-  margin: 0;
-}
-.back ul li {
-  margin-top: 15px;
-  color: whitesmoke;
-}
-.back .botprice {
-  position: absolute;
-  bottom: 30px;
-  border-radius: 0 0 10px 10px;
-  text-align: center;
-  width: 100%;
-  padding: 15px;
-  color: whitesmoke;
-}
-.back .botprice h3 {
-  margin: 0px;
-}
-.return {
-  position: absolute;
-  width: 100%;
-  background-color: transparent;
-  border: none;
-  color: gray;
-  cursor: pointer;
-  padding: 5px;
-  bottom: 0;
-  border-radius: 0 0 10px 10px;
-  background-color: #dcdcdc;
-  font-size: 18px;
-}
-.return:focus {
-  outline: none;
 }
 </style>
