ee1b919f1eef1c8cf5dfa6a78dd59f8a922059e7
Seth Williamson <sethjwilliamson@gmail.com>
2023-01-06 18:21:22 -0700

    Update DeckHighlight.vue

--- src/lib-components/DeckHighlight/src/DeckHighlight.vue
+++ src/lib-components/DeckHighlight/src/DeckHighlight.vue
@@ -9,7 +9,12 @@
       :cards="champions"
       :regions="regions"
     ></DeckHighlightCards>
-    <FontAwesomeIcon ref="eyeIcon" icon="eye" class="icon" />
+    <FontAwesomeIcon
+      ref="eyeIcon"
+      icon="eye"
+      class="icon"
+      @click="onEyeClick"
+    />
 
     <div ref="deckSummary" class="deck-summary-wrapper">
       <DeckSummary
@@ -19,6 +24,10 @@
         :cards-prop="cards"
       ></DeckSummary>
     </div>
+
+    <ModalItem v-model:show-modal="showModal">
+      <DeckPreview :cards="cards" :deck="deck" :deck-code="props.deckCode" />
+    </ModalItem>
   </div>
 </template>
 
@@ -26,12 +35,15 @@
 import {
   getCardsFromDeck,
   getDeckObjectFromCode,
+  getMostImportantCards,
   getRegions,
   isAChampion,
 } from '@/helpers/functions';
 import DeckSummary from '@/lib-components/Deck/DeckSummary';
 import DeckHighlightCards from '@/lib-components/DeckHighlightCards';
 import DeckHighlightRegions from '@/lib-components/DeckHighlightRegions';
+import DeckPreview from '@/lib-components/DeckPreview';
+import ModalItem from '@/lib-components/ModalItem';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import tippy from 'tippy.js';
 import { computed, onMounted, ref } from 'vue';
@@ -43,6 +55,7 @@ const deckSummary = ref<HTMLElement | null>(null);
 const eyeIcon = ref<InstanceType<typeof FontAwesomeIcon> | null>(null);
 
 const hasShown = ref(false);
+const showModal = ref(false);
 
 const deck = computed(() => {
   if (props.deck) {
@@ -70,7 +83,7 @@ const champions = computed(() => {
 const deckHighlightStyle = computed(() => {
   return {
     '--tippy-background': `url(https://lor.gg/storage/cards/full-art/${
-      champions.value[champions.value.length - 1].cardCode
+      getMostImportantCards(cards.value, deck.value, 1)[0].cardCode
     }.webp)`,
   };
 });
@@ -87,6 +100,10 @@ onMounted(() => {
     },
   });
 });
+
+function onEyeClick() {
+  showModal.value = true;
+}
 </script>
 
 <style scoped>
