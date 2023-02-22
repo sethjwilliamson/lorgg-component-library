59d797794496b29a7b859d3fa779bbb6df2de579
Seth Williamson <sethjwilliamson@gmail.com>
2023-02-21 21:14:18 -0700

    Change DecksTable to use LargeTable

--- src/lib-components/DecksTable/src/DecksTable.vue
+++ src/lib-components/DecksTable/src/DecksTable.vue
@@ -1,50 +1,65 @@
 <template>
-  <div class="table-wrapper">
-    <div class="decks-table table no-scrollbar">
-      <div class="heading-background-outer">
-        <div class="heading-background-absolute"></div>
-        <div class="heading-background-inner"></div>
-      </div>
-      <div class="heading wrapper">
-        <div class="heading-cell column-content">{{ t('general.deck') }}</div>
-        <div class="heading-cell column-content">
-          {{ t('general.matches') }}
-        </div>
-        <div class="heading-cell column-content">
-          {{ t('general.winrate') }}
-        </div>
-        <div class="heading-cell column-content">
-          {{ t('general.manaCurve') }}
-        </div>
-        <div class="heading-cell column-content">{{ t('general.cost') }}</div>
-      </div>
-      <DecksRow
-        v-for="(deckRow, index) in props.deckRows"
-        :key="deckRow.deckCode"
-        :deckcode="deckRow.deckCode"
-        :matches="deckRow.matches"
-        :wins="deckRow.wins"
-        :index="index + 2"
-      />
-    </div>
-  </div>
+  <LargeTable class="large-table" :heading-items="headingItems">
+    <DecksRow
+      v-for="(deckRow, index) in props.deckRows"
+      :key="deckRow.deckCode"
+      :deckcode="deckRow.deckCode"
+      :matches="deckRow.matches"
+      :wins="deckRow.wins"
+      :index="index + 2"
+    />
+  </LargeTable>
 </template>
 <script setup lang="ts">
 import DecksRow from '@/lib-components/DecksRow';
 import { DecksTableProps, decksTableProps } from './types';
 import { useI18n } from 'vue-i18n';
+import {
+  HeadingItem,
+  SortDirection,
+} from '@/lib-components/LargeTable/src/types';
+import LargeTable from '@/lib-components/LargeTable';
 const { t } = useI18n();
 
 const props: DecksTableProps = defineProps(decksTableProps);
+
+const headingItems: HeadingItem[] = [
+  {
+    id: 'deck',
+    isShown: true,
+    sortDirection: SortDirection.NOT_SORTABLE,
+    title: t('general.deck'),
+  },
+  {
+    id: 'matches',
+    isShown: true,
+    sortDirection: SortDirection.DOWN,
+    title: t('general.matches'),
+  },
+  {
+    id: 'winrate',
+    isShown: true,
+    sortDirection: SortDirection.NEITHER,
+    title: t('general.winrate'),
+  },
+  {
+    id: 'manaCurve',
+    isShown: true,
+    sortDirection: SortDirection.NEITHER,
+    title: t('general.manaCurve'),
+  },
+  {
+    id: 'cost',
+    isShown: true,
+    sortDirection: SortDirection.NEITHER,
+    title: t('general.cost'),
+  },
+];
 </script>
 
 <style scoped>
-.decks-table {
-  --background-color: var(--color-background-1);
-  background-color: var(--background-color);
-  gap: 10px;
-  grid-auto-rows: 50px;
-  grid-template-columns: repeat(5, 1fr) 50px;
-  padding: 0 4px;
+.large-table {
+  --table-grid-auto-rows: 50px;
+  --table-grid-template-columns: repeat(5, 1fr) 50px;
 }
 </style>
