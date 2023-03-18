1634cf8000a4c3378c8c165d20be529f3694c52a
Seth Williamson <sethjwilliamson@gmail.com>
2023-03-17 20:27:28 -0600

    Move background card logic to app.css

--- src/lib-components/ArchetypesRow/src/ArchetypesRow.vue
+++ src/lib-components/ArchetypesRow/src/ArchetypesRow.vue
@@ -5,16 +5,16 @@
       class="archetypes-row-wrapper wrapper"
       :style="{ '--index': props.index }"
     >
-      <div class="archetypes-row-background-wrapper">
-        <div class="archetypes-row-background">
+      <div class="table-card-row-background-wrapper">
+        <div class="table-card-row-background">
           <div
             v-for="backgroundCard in backgroundCards"
             :key="backgroundCard"
             class="background-card-wrapper"
             :style="{ '--background-image': `url(${backgroundCard})` }"
           >
-          <div class="background-card"></div>
-        </div>
+            <div class="background-card"></div>
+          </div>
         </div>
       </div>
       <div class="archetypes-row wrapper">
@@ -74,86 +74,7 @@ const backgroundCards = computed(() => {
 </script>
 
 <style scoped>
-.archetypes-row-background-wrapper {
-  grid-row: var(--index);
-  grid-column: 1 / -1;
-  position: relative;
-}
-
-.archetypes-row-background {
-  background-color: var(--color-background-2);
-  border: var(--color-3) solid 2px;
-  border-radius: 20px;
-  display: flex;
-  overflow: hidden;
-  position: absolute;
-  inset: 0;
-  transition: border-color 0.2s;
-}
-
-a.wrapper:hover .archetypes-row-background {
+a.wrapper:hover .table-card-row-background {
   border-color: #816f0d;
-}
-
-.background-card-wrapper {
-  flex-grow: 1;
-}
-
-.background-card {
-  --overlap-margin: 100px;
-  background-image: var(--background-image);
-  background-size: cover;
-  background-position: center 25%;
-  opacity: 40%;
-  height: 100%;
-  margin-left: calc( -1 * var(--overlap-margin));
-  margin-right: calc( -1 * var(--overlap-margin));
-
-  -webkit-mask-image: linear-gradient(
-    to right,
-    transparent 0%,
-    black calc(0% + (2 * var(--overlap-margin))),
-    black calc(100% - (2 * var(--overlap-margin))),
-    transparent 100%
-  );
-  mask-image: linear-gradient(
-    to right,
-    transparent 0%,
-    black calc(0% + (2 * var(--overlap-margin))),
-    black calc(100% - (2 * var(--overlap-margin))),
-    transparent 100%
-  );
-}
-
-.background-card-wrapper:first-child > .background-card {
-  -webkit-mask-image: linear-gradient(
-    to right,
-    black 0%,
-    black calc(100% - (2 * var(--overlap-margin))),
-    transparent 100%
-  );
-  mask-image: linear-gradient(
-    to right,
-    black 0%,
-    black calc(100% - (2 * var(--overlap-margin))),
-    transparent 100%
-  );
-  margin-left: 0;  
-}
-
-.background-card-wrapper:last-child > .background-card {
-  -webkit-mask-image: linear-gradient(
-    to right,
-    transparent 0%,
-    black calc(0% + (2 * var(--overlap-margin))),
-    black 100%
-  );
-  mask-image: linear-gradient(
-    to right,
-    transparent 0%,
-    black calc(0% + (2 * var(--overlap-margin))),
-    black 100%
-  );
-  margin-right: 0;
 }
 </style>
